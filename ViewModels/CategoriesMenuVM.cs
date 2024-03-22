9f2366f538ca9ccc1c2bb8d1c7256461181ffcd9
anhtuanpc2205 <147957110+anhtuanpc2205@users.noreply.github.com>
2024-03-22 22:05:31 +0700

    Create CategoriesMenuComponent step 1

---
+++ ViewModels/CategoriesMenuVM.cs
@@ -0,0 +1,26 @@
+﻿using BookStore2024.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BookStore2024.ViewModels
+{
+	public class CategoriesMenuVM
+	{
+		//private readonly BookStoreContext DBContext;
+
+		public int CategoryId { get; set; }
+
+		public string CategoryName { get; set; }
+
+		public int Quantity {  get; set; }
+
+		//public CategoriesMenuVM(BookStoreContext db,int cate_id, string cate_name) 
+		//{  
+		//	DBContext = db; 
+		//	CategoryId = cate_id;
+		//	CategoryName = cate_name;
+		//	Quantity = db.ViewBookDetails.Where(bd =>  bd.CategoryId == CategoryId).Count();
+
+		//}
+	}
+}
