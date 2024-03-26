6317e0192a6b17c35deb5220c07e2e481569d4f9
anhtuan2205 <57052425+anhtuan2205@users.noreply.github.com>
2024-03-26 09:20:33 +0700

    Add some attribute to ProductVM

--- ViewModels/ProductVM.cs
+++ ViewModels/ProductVM.cs
@@ -2,12 +2,26 @@
 {
 	public class ProductVM
 	{
-		public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public string? ProductName { get; set; }
-		public string? ProductImg { get; set; }
-		public string? AuthorName { get; set; }
-		public decimal Price { get; set; }
+		public string? ProductName { get; set; }
+        public int GenreId { get; set; }
+        public string GenreName { get; set; } = null!;
+        public string CategoryName { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
 		public decimal? Discount { get; set; } = 0;
-	}
+        public string? Publisher { get; set; }
+        public string? ProductImg { get; set; }
+        public string? ProductDescription { get; set; }
+        public string? AuthorName { get; set; }
+        public string? Language { get; set; }
+        public int? Pages { get; set; }
+        public string? IllustrationsNote { get; set; }
+        public string? Isbn10 { get; set; }
+        public string? Isbn13 { get; set; }
+        public int FormatId { get; set; } = 1;
+        public string FormatName { get; set; } = null!;
+        public int StockQuantity { get; set; }
+        public int? Views { get; set; }
+
+    }
 }
